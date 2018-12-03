1edef33836a817ed52e26500cdbc1fb6ea53257c
dat <datdta95@gmail.com>
2018-12-03 09:30:55 +0700

    model employ

---
+++ src/main/java/com/thinkpad/datvutienwbdemployeemanagement/model/Employee.java
@@ -0,0 +1,94 @@
+package com.thinkpad.datvutienwbdemployeemanagement.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String gender;
+    private String dayOfBirth;
+    private String phoneNumber;
+    private String idCard;
+    private String email;
+    private String address;
+    private Integer grEmployId;
+
+    public Employee(){}
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDayOfBirth() {
+        return dayOfBirth;
+    }
+
+    public void setDayOfBirth(String dayOfBirth) {
+        this.dayOfBirth = dayOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getGrEmployId() {
+        return grEmployId;
+    }
+
+    public void setGrEmployId(Integer grEmployId) {
+        this.grEmployId = grEmployId;
+    }
+}
