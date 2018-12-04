1112103afd530aa06b2fffe33cd29f98952aa525
dat <datdta95@gmail.com>
2018-12-04 18:13:06 +0700

    group repo

--- src/main/java/com/thinkpad/datvutienwbdemployeemanagement/repository/GroupEmployeeRepository.java
+++ src/main/java/com/thinkpad/datvutienwbdemployeemanagement/repository/GroupEmployeeRepository.java
@@ -1,7 +1,7 @@
-//package com.thinkpad.datvutienwbdemployeemanagement.repository;
-//
-//import com.thinkpad.datvutienwbdemployeemanagement.model.GroupEmployee;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface GroupEmployeeRepository extends CrudRepository<GroupEmployee,Integer> {
-//}
+package com.thinkpad.datvutienwbdemployeemanagement.repository;
+
+import com.thinkpad.datvutienwbdemployeemanagement.model.GroupEmployee;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GroupEmployeeRepository extends CrudRepository<GroupEmployee,Integer> {
+}
