398ca2181d7c41b1f70551c86a2b0e96a3c5a723
dat <datdta95@gmail.com>
2018-12-04 18:12:19 +0700

    Group service

--- src/main/java/com/thinkpad/datvutienwbdemployeemanagement/service/GroupEmployeeService.java
+++ src/main/java/com/thinkpad/datvutienwbdemployeemanagement/service/GroupEmployeeService.java
@@ -1,14 +1,14 @@
-//package com.thinkpad.datvutienwbdemployeemanagement.service;
-//
-//import com.thinkpad.datvutienwbdemployeemanagement.model.GroupEmployee;
-//
-//import java.util.Optional;
-//
-//public interface GroupEmployeeService {
-//    Iterable<GroupEmployee> findAll();
-//    Optional<GroupEmployee> findById(Integer id);
-//
-//    void save(GroupEmployee groupEmployee);
-//
-//    void remove(GroupEmployee groupEmployee);
-//}
+package com.thinkpad.datvutienwbdemployeemanagement.service;
+
+import com.thinkpad.datvutienwbdemployeemanagement.model.GroupEmployee;
+
+import java.util.Optional;
+
+public interface GroupEmployeeService {
+    Iterable<GroupEmployee> findAll();
+    Optional<GroupEmployee> findById(Integer id);
+
+    void save(GroupEmployee groupEmployee);
+
+    void remove(GroupEmployee groupEmployee);
+}
--- src/main/java/com/thinkpad/datvutienwbdemployeemanagement/service/GroupEmployeeServiceImpl.java
+++ src/main/java/com/thinkpad/datvutienwbdemployeemanagement/service/GroupEmployeeServiceImpl.java
@@ -1,33 +1,33 @@
-//package com.thinkpad.datvutienwbdemployeemanagement.service;
-//
-//import com.thinkpad.datvutienwbdemployeemanagement.model.GroupEmployee;
-//import com.thinkpad.datvutienwbdemployeemanagement.repository.GroupEmployeeRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import java.util.Optional;
-//
-//public class GroupEmployeeServiceImpl implements GroupEmployeeService{
-//        @Autowired
-//        private GroupEmployeeRepository groupEmployeeRepository;
-//
-//        @Override
-//        public Iterable<GroupEmployee> findAll() {
-//            return groupEmployeeRepository.findAll();
-//        }
-//
-//        @Override
-//        public Optional<GroupEmployee> findById(Integer id) {
-//                return groupEmployeeRepository.findById(id);
-//        }
-//
-//
-//        @Override
-//        public void save(GroupEmployee groupEmployee) {
-//                groupEmployeeRepository.save(groupEmployee);
-//        }
-//
-//        @Override
-//        public void remove(GroupEmployee groupEmployee) {
-//                groupEmployeeRepository.delete(groupEmployee);
-//        }
-//}
+package com.thinkpad.datvutienwbdemployeemanagement.service;
+
+import com.thinkpad.datvutienwbdemployeemanagement.model.GroupEmployee;
+import com.thinkpad.datvutienwbdemployeemanagement.repository.GroupEmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class GroupEmployeeServiceImpl implements GroupEmployeeService{
+        @Autowired
+        private GroupEmployeeRepository groupEmployeeRepository;
+
+        @Override
+        public Iterable<GroupEmployee> findAll() {
+            return groupEmployeeRepository.findAll();
+        }
+
+        @Override
+        public Optional<GroupEmployee> findById(Integer id) {
+                return groupEmployeeRepository.findById(id);
+        }
+
+
+        @Override
+        public void save(GroupEmployee groupEmployee) {
+                groupEmployeeRepository.save(groupEmployee);
+        }
+
+        @Override
+        public void remove(GroupEmployee groupEmployee) {
+                groupEmployeeRepository.delete(groupEmployee);
+        }
+}
