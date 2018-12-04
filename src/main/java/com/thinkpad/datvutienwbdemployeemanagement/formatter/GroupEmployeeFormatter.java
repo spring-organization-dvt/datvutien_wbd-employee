f5432e0271f716438fa9d90006f4dbcedf8d645b
dat <datdta95@gmail.com>
2018-12-04 18:11:59 +0700

    formatter

--- src/main/java/com/thinkpad/datvutienwbdemployeemanagement/formatter/GroupEmployeeFormatter.java
+++ src/main/java/com/thinkpad/datvutienwbdemployeemanagement/formatter/GroupEmployeeFormatter.java
@@ -1,32 +1,33 @@
-//package com.thinkpad.datvutienwbdemployeemanagement.formatter;
-//
-//import com.thinkpad.datvutienwbdemployeemanagement.model.GroupEmployee;
-//import com.thinkpad.datvutienwbdemployeemanagement.service.GroupEmployeeService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.format.Formatter;
-//import org.springframework.stereotype.Component;
-//
-//import java.text.ParseException;
-//import java.util.Locale;
-//
-//@Component
-//public class GroupEmployeeFormatter implements Formatter<GroupEmployee> {
-//    private GroupEmployeeService groupEmployeeService;
-//
-//    @Autowired
-//    public GroupEmployeeFormatter(GroupEmployeeService groupEmployeeService) {
-//        this.groupEmployeeService = groupEmployeeService;
-//    }
-//
-//    @Override
-//    public GroupEmployee parse(String text, Locale locale) throws ParseException {
-////        Integer integer = Integer.valueOf(text);
-//        return groupEmployeeService.findById(text);
-//    }
-//
-//    @Override
-//    public String print(GroupEmployee object, Locale locale) {
-//        return "[" + object.getId() + ", " + object.getName() + "]";
-//    }
-//
-//}
+package com.thinkpad.datvutienwbdemployeemanagement.formatter;
+
+import com.thinkpad.datvutienwbdemployeemanagement.model.GroupEmployee;
+import com.thinkpad.datvutienwbdemployeemanagement.service.GroupEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Optional;
+
+@Component
+public class GroupEmployeeFormatter implements Formatter<Optional<GroupEmployee>> {
+    private GroupEmployeeService groupEmployeeService;
+
+    @Autowired
+    public GroupEmployeeFormatter(GroupEmployeeService groupEmployeeService) {
+        this.groupEmployeeService = groupEmployeeService;
+    }
+
+
+    @Override
+    public Optional<GroupEmployee> parse(String text, Locale locale) throws ParseException {
+        return groupEmployeeService.findById(Integer.parseInt(text));
+    }
+
+    @Override
+    public String print(Optional<GroupEmployee> object, Locale locale) {
+        return "[" + object.get().getId() + ", " + object.get().getName() + "]";
+
+    }
+}
